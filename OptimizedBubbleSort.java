f7d1aa075b70faec13681050e9e4904309e7c75a
gsimon1720 <gaveshfernando17@gmail.com>
2026-01-13 21:25:18 +0530

    Add files via upload

---
+++ OptimizedBubbleSort.java
@@ -0,0 +1,40 @@
+class OptimizedBubbleSort {
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no swaps, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        bubbleSort(arr);
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
---
+++ TowerOfHanoi.java
@@ -0,0 +1,18 @@
+class TowerOfHanoi {
+
+    static void solveHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + fromRod + " to " + toRod);
+            return;
+        }
+
+        solveHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+        solveHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String[] args) {
+        int n = 3; // number of disks
+        solveHanoi(n, 'A', 'C', 'B');
+    }
+}
